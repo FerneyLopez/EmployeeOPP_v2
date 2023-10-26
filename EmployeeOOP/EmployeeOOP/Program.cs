ab5000eee3770d6170e8d2346d4578336c5b6c3b
FERNEY ORLANDO LOPEZ COPETE <ferneylopez188523@correo.itm.edu.co>
2023-10-25 23:07:32 -0500

    Creación de excepciones y validaciones.

--- EmployeeOOP/EmployeeOOP/Program.cs
+++ EmployeeOOP/EmployeeOOP/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Ubicaciónde clases a utilizar.
+using EmployeeOOP.Classes;
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("OOP  APLICATION!");
+Console.WriteLine("----------------");
+
+//Recordemos que la clase program.cs es la primera que se ejectuta.
+
+try
+{
+    //Declaración de variables
+    int day, month, year;
+
+    Console.WriteLine("OOP APPLICATION");
+    Console.WriteLine("---------------");
+
+    Console.Write("Ingresar el día: ");
+    day = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Ingresar el mes: ");
+    month = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Ingresar el año: ");
+    year = Convert.ToInt32(Console.ReadLine());
+
+    //Inicialización de la clase Date
+    Date dateObject = new Date(day, month, year);
+    Console.WriteLine("\n");
+    Console.WriteLine(dateObject.ToString()); // así imprimo el objeto para poder verlo. 
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message); // desde acá controlo las excepciones
+}
+
